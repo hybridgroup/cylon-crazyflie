40f41b5556d88e6db91881061da2cbc689c0fd5b
Andrew Stewart <andrew@stwrt.ca>
2014-06-25 13:00:26 -0700

    Improve specs, remove unnnecessary command

--- test/specs/commands.spec.js
+++ test/specs/commands.spec.js
@@ -4,10 +4,10 @@ var Commands = source('commands');
 
 describe('Cylon.Crazyflie.Commands', function() {
   it('is an array of string commands', function() {
-    for (var i = 0; i < Commands.length; i++) {
-      var command = Commands[i];
+    expect(Commands).to.be.an('array');
+
+    Commands.forEach(function(command) {
       expect(command).to.be.a('string');
-    }
+    });
   });
 });
-
--- test/specs/driver.spec.js
+++ test/specs/driver.spec.js
@@ -3,20 +3,49 @@
 var Driver = source('driver'),
     Commands = source('commands');
 
+var Cylon = require('cylon');
+
 describe('Cylon.Drivers.Crazyflie', function() {
-  var crazyflie = new Driver({ device: {} });
+  var driver;
 
-  it("exposes a 'commands' method exposing all available commands", function() {
-    expect(crazyflie.commands).to.be.eql(Commands);
+  beforeEach(function() {
+    stub(Driver.prototype, 'proxyMethods');
+    driver = new Driver({
+      device: {}
+    });
   });
 
-  it("exposes a 'stop' method to disconnect from the Crazyflie", function() {
-    expect(crazyflie.stop).to.be.a('function');
+  afterEach(function() {
+    Driver.prototype.proxyMethods.restore();
   });
 
-  it("exposes a 'setParam' method to proxy to the connection", function() {
-    crazyflie.connection = { setParam: spy() };
-    crazyflie.setParam("testing", "true");
-    expect(crazyflie.connection.setParam).to.be.calledWith("testing", "true");
+  it("subclasses Cylon.Driver", function() {
+    expect(driver).to.be.an.instanceOf(Cylon.Driver);
+    expect(driver).to.be.an.instanceOf(Driver);
+  });
+
+  describe("constructor", function() {
+    it("sets @commands to the Crazyflie commands list", function() {
+      expect(driver.commands).to.be.eql(Commands);
+    });
+
+    it("proxies Crazyflie methods to the connection", function() {
+      expect(driver.proxyMethods).to.be.calledWith(
+        Commands,
+        driver.connection,
+        driver
+      );
+    });
+  });
+
+  describe("#setParam", function() {
+    beforeEach(function() {
+      driver.connection = { setParam: spy() }
+    });
+
+    it("passes the param and value to the connection", function() {
+      driver.setParam("hello", "world");
+      expect(driver.connection.setParam).to.be.calledWith("hello", "world");
+    })
   });
 });
